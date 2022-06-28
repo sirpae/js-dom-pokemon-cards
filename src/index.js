81b295062442a0e069b2c4042bc716833d21ad32
p <101421945+sirpae@users.noreply.github.com>
2022-06-28 17:35:18 +0100

    Not Completed. Recent Push.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
+const cardsUL = document.querySelector(".cards");
 
-console.log(data);
+for (let i = 0; i < data.length; i++) {
+  console.log("Pokemon at i", data[i]);
+  const pokemon = data[i];
+  const cardLI = document.createElement('li')
+  cardLI.classList.add('card')
+  
+  const h2 = document.createElement("h2");
+  h2.innerText = pokemon.name;
+  
+  h2.classList.add('card--title')
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  const img = document.createElement('img')
+  img.setAttribute('src', pokemon.sprites.other['official-artwork'].front_default)
+  cardLI.appendChild(img)
+
+  cardsUL.append(cardLI)
+  cardLI.appendChild(h2)
+
+}
+
+
+// const cardLI = document.createElement('li')
+// add CSS class
+// cardLI.classList.add('card')
+
+
+// create heading
+const title = document.createElement('h2')
+// add CSS class
+title.classList.add('card--title')
+
+// set the text
+// title.innerText = pokemon.name
+
+// add the title into the card
+cardLI.append(title)
+
+
+
+
+
+
