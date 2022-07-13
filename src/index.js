2cb34e7544c2aa70d94a7fcee448a7d11af62f73
p <101421945+sirpae@users.noreply.github.com>
2022-07-13 10:31:20 +0100

    Finished

--- src/index.js
+++ src/index.js
@@ -5,20 +5,32 @@ for (let i = 0; i < data.length; i++) {
   const pokemon = data[i];
   const cardLI = document.createElement('li')
   cardLI.classList.add('card')
+  cardLI.style.listStyleType = 'none'
+
   
   const h2 = document.createElement("h2");
   h2.innerText = pokemon.name;
-  
   h2.classList.add('card--title')
 
   const img = document.createElement('img')
   img.setAttribute('src', pokemon.sprites.other['official-artwork'].front_default)
   cardLI.appendChild(img)
-
   cardsUL.append(cardLI)
   cardLI.appendChild(h2)
 
+  // nested for loop for card data
+  for (let j = 0; j < data[i].stats.length; j++) {
+    const pokemon = document.createElement('li')
+    cardLI.append(pokemon)
+    pokemon.style.textTransform = 'upperCase'
+    pokemon.style.listStyleType = 'none'
+    pokemon.innerText = data[i].stats[j].stat.name + ' ' + data[i].stats[j].base_stat 
+  }
+
 }
+
+
+
 
 
 // const cardLI = document.createElement('li')
